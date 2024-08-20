98e7904d8065acbe8c4519daee0c30bc05ac8433
Marlon Braga <mbraga@turimbr.com>
2024-08-20 10:21:05 -0300

    clean code

--- CodeCase.IntegratedTest/UnitTest1.cs
+++ CodeCase.IntegratedTest/UnitTest1.cs
@@ -1,3 +1,16 @@
+/*
+TODO: List 0/10
+1.[ ] injeção de dependência em projeto de teste integrado
+2.[ ] docker compose para criação de banco de dados relacional (MsSQL)
+3.[ ] implementação de Entity Framework Core
+4.[ ] classes com funcionalidades Controllers, Repository (CRUD)
+5.[ ] testContainers para simular ambiente de teste do MsSQL
+6.[ ] docker compose para criação de fila de mensagens (RabbitMQ)
+7.[ ] testContainers para simular ambiente de teste do RabbitMQ
+8.[ ] 
+9.[ ] 
+0.[ ] 
+ */
 namespace CodeCase.IntegratedTest
 {
     public class UnitTest1
